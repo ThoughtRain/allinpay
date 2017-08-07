eff3e55b308a6667debde541b889af49b6d1683d
rui <1807520106@qq.com>
2017-08-07 10:55:11 +0800

    连接数据库和导入必要的包

--- src/com/allinpaly/service/test.java
+++ src/com/allinpaly/service/test.java
@@ -1,19 +1,30 @@
 package com.allinpaly.service;
 
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/8/7.
  */
 @Controller
 public class test {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @ResponseBody
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String x() {
-        return "1111";
+        String sql = "select * from table_uers";
+        List map = jdbcTemplate.queryForList(sql);
+        Gson gson = new Gson();
+        return gson.toJson(map);
     }
 
 }

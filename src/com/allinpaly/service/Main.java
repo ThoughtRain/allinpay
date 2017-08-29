5cb667941615182522c8fbe1b287ff6798ffec2e
rui <1807520106@qq.com>
2017-08-29 16:10:08 +0800

    修改

--- src/com/allinpaly/service/Main.java
+++ src/com/allinpaly/service/Main.java
@@ -4,7 +4,15 @@ package com.allinpaly.service;
  * Created by prarui on 2017/8/7.
  */
 public class Main {
-    public static void main(String arg[]) {
 
+    public static void main(String arg[]) {
+        String x[] = {"a", "b", "c"};
+        for (int index = 0; index < x.length; index++) {
+            for (int i = 0; i < x.length; i++) {
+                {
+                    System.out.println(x[index] + x[i]);
+                }
+            }
+        }
     }
 }
